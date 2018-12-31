8c4957e0996e3bba2b0d742aac6f07be3629a7f2
nitinmak <nitinmakwana539@gmail.com>
2019-01-01 01:56:04 +0530

    sceem
    banvvu

--- js/my-app.js
+++ js/my-app.js
@@ -1,12 +1,12 @@
 // Initialize app
-var myApp = new Framework7();
+var app = new Framework7();
 
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
+var mainView = app.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
@@ -14,7 +14,6 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
-	
 });
    
 
@@ -25,42 +24,61 @@ $$(document).on('click', '#favorites', function (e) {
  $$(document).on('click', '#about', function (e) {
    
  });
-    
+
  $$(document).on('click', '#settings', function (e) {
- 	document.getElementById('x').className = 'panel panel-left panel-reveal';
- 	document.getElementById('formx').className = 'mainbody';
+
  });
 // Now we need to run the code that will be executed only for About page.
 
+// app.onPageInit('loginscreen', function (page) {
+// //     // Do something here for "about" page
+	
+// })
+
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-	var tttoken = "v7nirxu0scv0eistepradr";
-	$$.ajax({
-				 url: "https://unofox.xyz/kishan/10eleven/system_pin_history/",
-				cache: false,
-				headers : {"Access-Control-Allow-Credentials":true,"Access-Control-Allow-Origin":"https://unofox.xyz"},
-				type: "POST",
-				dataType: "json",
-				data: { 'varsete': tttoken ,memberid : '100088'},
-				success: function(response){
+app.onPageInit('about', function (page) {
+//     // Do something here for "about" page
+// 	var tttoken = "v7nirxu0scv0eistepradr";
+// 	$$.ajax({
+// 				 url: "https://unofox.xyz/kishan/10eleven/system_pin_history/",
+// 				cache: false,
+// 				headers : {"Access-Control-Allow-Credentials":true,"Access-Control-Allow-Origin":"https://unofox.xyz"},
+// 				type: "POST",
+// 				dataType: "json",
+// 				data: { 'varsete': tttoken ,memberid : '100088'},
+// 				success: function(response){
 					
-					var htmlx = "";
-					$$.each(response["data"], function (key, value) {
-							htmlx += " <tr>  <td style='color:black;' class='label-cell'>" + value.date + "</td> <td style='color:black;' class='label-cell'>" + value.memberid + "</td> <td style='color:black;' class='numeric-cell'>" + value.pinnumber + "</td></tr> ";
-						});
-					$$("#coins_table").html(htmlx);
+// 					var htmlx = "";
+// 					var htmly = "";
+// 					$$.each(response["data"], function (key, value) {
+// 							htmly += "<div class='list-group car'  > <div class='list-group-title'> Date : "+value.date +"</div> <div class='item-after'> memberid : " + value.memberid +"</div>  <div class='item-subtitle'> pin number"+ value.pinnumber +"</div></div>";
+
+// 							// htmlx += " <tr>  <td style='color:black;' class='label-cell'>" + value.date +
+// 							//  "</td><td style='color:black;' class='label-cell'>" + value.memberid + 
+// 							//  "</td> <td style='color:black;' class='numeric-cell'>" + value.pinnumber + 
+// 							//  "</td></tr> ";
+// 						});
+// 					$$("#mydynamic").html(htmly);
+
+// 					$$("#coins_table").html(htmlx);
+
+// 					// <div class="item-title-row">
+// 					// 	            <div class="item-title">Facebook</div>
+// 					// 	            <div class="item-after">17:14</div>
+// 					// 	          </div>
+// 					// 	          <div class="item-subtitle">ersetwetwtwtw</div>
+// 					// 	          <div class="item-text"></div>
+// 					//console.log(htmlx);	
+// 					//app.alert(' got the response');
+// 				},
+// 				error: function(responsex){
+// 					console.log(responsex);
+// 					app.alert(responsex.status+' got an error');
 					
-					//console.log(htmlx);	
-					//myApp.alert(' got the response');
-				},
-				error: function(responsex){
-					console.log(responsex);
-					myApp.alert(responsex.status+' got an error');
-					
-				}
-			});
+// 				}
+// 			});
 })
+
 
 // Option 2. Using one 'pageInit' event handler for all pages:
 $$(document).on('pageInit', function (e) {
@@ -68,13 +86,13 @@ $$(document).on('pageInit', function (e) {
     var page = e.detail.page;
 
     if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
+    //     // Following code will be executed for page with data-page attribute equal to "about"
         
     }
 })
 
-// Option 2. Using live 'pageInit' event handlers for each page
+// // Option 2. Using live 'pageInit' event handlers for each page
 $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
-   // myApp.alert('Here comes About page');
+   // app.alert('Here comes About page');
 })
